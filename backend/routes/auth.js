e4f0747612514cfd96a6b19959c9f2d74566a48f
Khalid <boulkrim.khalid@protonmail.com>
2025-06-05 05:08:18 +0200

    Add the auth.js file with basic post to auth google

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,33 @@
-// import { createUser, getAllUsers } from "../db/db.js";
+import { createUser, getAllUsers } from "../db/db.js";
+import {OAuth2Client} from 'google-auth-library';
 
-// export default function authGoogle(fastify, options, done)
-// {
-//     fastify.post('/auth/google', async(request, reply) =>
-//     {
-        
-//     })
-// }
+const client = new OAuth2Client();
+
+
+export default function authGoogle(fastify, options, done)
+{
+    fastify.post('/auth/google', async(request, reply) =>
+    {
+        try
+        {
+            const client_id = "632484486903-vm1hfg66enqfkffsmlhih0au506obuch.apps.googleusercontent.com"
+            const {token} = request.body;
+            const ticket = await client.verifyIdToken({ idToken: token, audience: client_id});
+            const payload = ticket.getPayload();
+            
+            const googleId = payload.sub;
+            const email = payload.email;
+            const fullname = payload.name;
+            const firstName = payload.given_name;
+            const lastName = payload.family_name;
+            const picture = payload.picture;
+
+            reply.send({ user: { googleId, fullname, firstName, lastName, email, picture } });
+        }
+        catch(err)
+        {
+            reply.status(401).send({ success: false, message: "Token Google invalide" });
+        }
+    })
+    done();
+}
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,30 @@
 import Fastify from 'fastify';
 import { createTable } from './db/db.js';
 import userRoutes from './routes/user.js';
+import authGoogle from './routes/auth.js'
 
 const fastify = Fastify();
 
 fastify.register(userRoutes);
+fastify.register(authGoogle);
+
+
+// CORS simple
+fastify.addHook('preHandler', async (request, reply) => {
+  reply.header('Access-Control-Allow-Origin', '*')
+  reply.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  reply.header('Access-Control-Allow-Headers', 'Content-Type')
+  
+  if (request.method === 'OPTIONS') {
+    return reply.send()
+  }
+})
+
+// Routes
+fastify.get('/', async () => {
+  return { message: 'Backend OK!' }
+})
+
 
 const startServer = async() =>
 {
