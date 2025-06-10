bd0d5b4a876d098e171b177f83fdfd1cb16b6f20
lydidou <lydiaaboudjelal@gmail.com>
2025-06-10 02:44:31 +0200

    merging working front and back + fixed some issues with spa and login()

--- server/src/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const userController = require('../controllers/userController');
-async function userRoutes(fastify, opts) {
-  fastify.post('/users', userController.postUsers);
-  fastify.get("/users", userController.getUsers)
-}
-
-module.exports = userRoutes;
