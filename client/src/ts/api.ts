58e7c219b2bad68c6ce67ee24c6af9e00ed57db2
lydid0u <lydiaaboudjelal@gmail.com>
2025-07-15 19:52:41 +0200

    Refactor user profile management and API interactions
    - Removed old profile.ts file and replaced it with a new implementation that includes user data handling, username and avatar change functionalities.
    - Introduced new api.ts file for handling user data submission to the backend.
    - Updated password.ts to include password change and reset functionalities.
    - Modified tv-controls.js to change navigation routes.
    - Removed friends.html and integrated its functionality into the new profile management structure.
    - Updated spa.ts to include new routes and functionalities for user profile management.

---
+++ client/src/ts/api.ts
@@ -0,0 +1,49 @@
+interface UserData {
+    [key: string]: any;
+}
+
+async function sendUserDataToBackend(userData: UserData): Promise<void> {
+    try {
+            const response = await fetch('http://localhost:3000/auth/google', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData)
+        });
+
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP: ${response.status}`);
+        }
+        
+        // on recupere la reponse du backend, 
+        const result: { token?: string } = await response.json();
+        console.log("Réponse du backend:", result);
+
+        // save dans localStorage pour la session locale
+        localStorage.setItem('googleUser', JSON.stringify(userData));
+        localStorage.setItem('isAuthenticated', 'true');
+        if (result.token) 
+            localStorage.setItem('jwtToken', result.token);
+        
+        displayUserInfo(userData);
+        if (window.SPA && typeof SPA.navigateTo === 'function') {
+            SPA.navigateTo('/dashboard');
+        }
+
+    } catch (error) {
+        console.error("Erreur lors de l'envoi au backend:", error);
+        
+        localStorage.removeItem('googleUser');
+        localStorage.removeItem('isAuthenticated');
+        localStorage.removeItem('jwtToken');
+
+        if (typeof google !== 'undefined' && google.accounts) {
+            google.accounts.id.disableAutoSelect();
+        }
+        
+        if (window.SPA && typeof SPA.navigateTo === 'function') {
+            SPA.navigateTo('/login');
+        }
+    }
+}
